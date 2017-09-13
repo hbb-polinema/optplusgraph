1fb9d1f983d95836f28d40c7179c2c8072e222f4
habibieeddien <23515043@std.stei.itb.ac.id>
2017-09-13 10:32:57 +0700

    helper for tracing code development

---
+++ src/dev/js/helper.ts
@@ -0,0 +1,9 @@
+/**
+ * author: Habibie Ed Dien
+ * purpose: for tracking code
+ */
+
+export function tracking(i: number, my: string, name_file?: string) { //optional parameter `name_file`
+    name_file = name_file || '###'; // if `name_file` not set, than equal `###`
+    //console.log("langkah " + i + " : " + my + " - " + name_file);
+}
